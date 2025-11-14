0cd1b09c50606e9151c3fc95f9e6664129f3d178
helmigandi <sugandihelmi@gmail.com>
2025-11-14 08:30:02 +0700

    Add new post: create bootable in linux os

--- js/posts/linux.js
+++ js/posts/linux.js
@@ -1,15 +1,21 @@
 const linuxPosts = [
   {
-    date: "21 Oktober 2025",
-    title: "Learn Linux for Programmer",
+    date: "14 November 2025",
+    title: "Cara Membuat Bootable Flashdisk di Linux",
     category: "linux",
-    url: "../posts/linux/learn-linux-for-programmer.html",
+    url: "../posts/linux/2025/bootbale-linux.html",
   },
   {
     date: "04 November 2025",
     title: "yt-dlp: Tools Powerful untuk Download Video",
     category: "linux",
     url: "../posts/linux/2025/yt-dlp-tools.html",
+  },
+  {
+    date: "21 Oktober 2025",
+    title: "Learn Linux for Programmer",
+    category: "linux",
+    url: "../posts/linux/learn-linux-for-programmer.html",
   },
   // Add more Linux posts here...
 ];
